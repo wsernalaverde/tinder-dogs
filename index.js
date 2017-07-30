7098e4cda162ef5d0e13dc00fd1af013623e424a
William Serna <wsernalaverde@gmail.com>
2017-07-29 20:40:32 -0500

    funcionalidad renderizar mis me gustas desde el localStorage

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var tinder = (function () {
 
-    const url = "https://dog.ceo/api/breeds/image/random"
+    const url = 'https://dog.ceo/api/breeds/image/random';
     let dog = document.querySelector('.cont-img-dog');
+   
 
     let like = function (imageDog) {
         const btnLikes = document.getElementById('btnLike');
@@ -10,11 +11,11 @@ var tinder = (function () {
                 let myLikes = localStorage.getItem('likesDogs');
                 let newLike = JSON.parse(myLikes);
                 newLike.push(imageDog);
-                localStorage.setItem("likesDogs", JSON.stringify(newLike));
+                localStorage.setItem('likesDogs', JSON.stringify(newLike));
             } else {
                 let likesDogs = [];
                 likesDogs.push(imageDog);
-                localStorage.setItem("likesDogs", JSON.stringify(likesDogs));
+                localStorage.setItem('likesDogs', JSON.stringify(likesDogs));
             }
             location.reload();
         })
@@ -41,7 +42,43 @@ var tinder = (function () {
     }
 
     let myLikes = function(){
-        
+        const listMyLikes = document.querySelector('.row-contenido');   
+        if(localStorage.getItem('likesDogs')){
+            let arrayMyLikes = JSON.parse(localStorage.getItem('likesDogs'));
+            arrayMyLikes.map((dato)=>{
+                const mediaObject = document.createElement('div');
+                mediaObject.classList.add('cont-media-object');
+                mediaObject.appendChild(createThumbnail(dato));
+                mediaObject.appendChild(createExcerpt(dato));
+                listMyLikes.appendChild(mediaObject);
+            })
+        }else{
+            const mediaObject = document.createElement('div');
+            mediaObject.classList.add('cont-media-object');
+            const message = document.createElement('h2');
+            message.classList.add('title-no-likes');
+            message.innerHTML = 'No tienes me gustas aún. Desplázate a ver los perfiles'
+            mediaObject.appendChild(message);
+            listMyLikes.appendChild(mediaObject);
+        }
+    }
+
+    let createThumbnail = function(imageURL){
+        const contThumbnail= document.createElement('div');
+        contThumbnail.classList.add('thumbnail-dog');
+        contThumbnail.appendChild(loadingImage(imageURL));
+        return contThumbnail;
+    }
+
+    let createExcerpt = function(imageURL){
+        const contExcerpt = document.createElement('div');
+        contExcerpt.classList.add('excerpt-dog');
+        const titleExcerpt = document.createElement('h2');
+        let name = imageURL.split('/');
+        titleExcerpt.innerHTML = name[5];
+        contExcerpt.appendChild(titleExcerpt);
+        contExcerpt.innerHTML += '<small>Medellín - Colombia</small>';
+        return contExcerpt;
     }
 
     let init = function () {
@@ -50,11 +87,14 @@ var tinder = (function () {
                 return response.json()
             })
             .then(function (json) {
-                dog.appendChild(loadingImage(json.message));
-                loadingName(json.message);
-                like(json.message);
-                dislike();
-                myLikes();
+                if(document.querySelector('.row-contenido')){
+                    myLikes();
+                }else{
+                    dog.appendChild(loadingImage(json.message));
+                    loadingName(json.message);
+                    like(json.message);
+                    dislike();
+                }            
             })
     }
 
